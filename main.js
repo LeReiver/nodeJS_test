e41872227cf18b7165a556b4784e90761a49ca93
Michael Le-Reiver <m.reiver@gmail.com>
2017-05-10 15:40:36 -0700

    adding main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by mike on 5/10/17.
+ */
+var http = require("http");
+
+http.createServer(function(request, response) {
+   // Send HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'}); 
+   
+   // Send the response body as "Hello Node!"
+    response.end('Hello Node\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
